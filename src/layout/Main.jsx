4f2402a8ceaa2877af5258c6161fa802086ecb61
mehrabhossain1 <mehrab.munna00@gmail.com>
2023-12-26 22:58:46 +0600

    basic setup

---
+++ src/layout/Main.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from 'react-router-dom'
+import Footer from '../common/Footer'
+import Navbar from '../common/Navbar'
+
+const Main = () => {
+  return (
+    <div>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  )
+}
+
+export default Main
